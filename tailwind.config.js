2aa185a903ff7535fa39010b51a5d53579bc96c6
Dhiman-Roy <dhiman.roy7075@gmail.com>
2024-09-24 13:00:24 +0600

    donation design complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "black-rgba": "rgba(17, 17, 17, 0.7)",
+      },
+    },
+  },
+  plugins: [],
+};
